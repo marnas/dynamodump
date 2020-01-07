76493b1bc4af285a794c24eeb74c0c56d1f97c81
Marco Santonastaso <m.santonastaso@protonmail.com>
2020-01-07 10:34:17 +0000

    Shared variables redefined globally

--- cmd/root.go
+++ cmd/root.go
@@ -43,12 +43,12 @@ var rootCmd = &cobra.Command{
 	Use:   "dynamodump",
 	Short: "AWS DynamoDB Backup and Restores",
 	Long: `
-Dynamodump allows for easier and cheaper actions and restores of DynamoDB Tables.
-Backups are compatible with the AWS DataPipeline functionality.
-	   
-It is also capable of restoring a actions from s3 to a given table both from this
-tool or from a actions generated using the AWS DataPipeline functionality.
-to quickly create a Cobra application.
+		Dynamodump allows for easier and cheaper actions and restores of DynamoDB Tables.
+		Backups are compatible with the AWS DataPipeline functionality.
+				
+		It is also capable of restoring a actions from s3 to a given table both from this
+		tool or from a actions generated using the AWS DataPipeline functionality.
+		to quickly create a Cobra application.
   `,
 }
 
@@ -66,9 +66,24 @@ func init() {
 		postInitCommands(rootCmd.Commands())
 	})
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
+	// Defining persistent flags, which
+	// will be global for the application.
+	rootCmd.PersistentFlags().StringVarP(&dynamoTableName, "dynamo-table-name", "t", "", "Name of the Dynamo table to actions. Environment variable: DYN_DYNAMO_TABLE_NAME (required)")
+	rootCmd.PersistentFlags().Int64VarP(&dynamoBatchSize, "dynamo-table-batch-size", "s", 1000, "Max number of records to read from the Dynamo table at once. Environment variable: DYN_DYNAMO_TABLE_BATCH_SIZE")
+	rootCmd.PersistentFlags().StringVarP(&dynamoTableAccountID, "dynamo-table-account-id", "x", "", "AccountID that will be used to access the dynamoDB")
+	rootCmd.PersistentFlags().StringVarP(&dynamoTableRegion, "dynamo-table-region", "o", "", "AWS region of the Dynamo table. Environment variable: DYN_DYNAMO_TABLE_REGION (required)")
+	rootCmd.PersistentFlags().Int64VarP(&waitTime, "dynamo-table-batch-wait-time", "w", 100, "Number of milliseconds to wait between batches. If a ProvisionedThroughputExceededException is encountered, the script will wait twice that amount of time before retrying. Environment variable: DYN_WAIT_TIME")
+	rootCmd.PersistentFlags().StringVarP(&roleAssumed, "assume-role", "g", "OrganizationAccountAccessRole", "Role that will be used to access the s3 Bucket")
+	rootCmd.PersistentFlags().StringVarP(&s3BucketAccountID, "s3-bucket-account-id", "e", "", "AccountID that will be used to access the s3 Bucket")
+	rootCmd.PersistentFlags().StringVarP(&s3BucketName, "s3-bucket-name", "b", "", "Name of the S3 bucket where to put the actions. Environment variable: DYN_S3_BUCKET_NAME (required)")
+	rootCmd.PersistentFlags().StringVarP(&s3BucketFolderName, "s3-bucket-folder-name", "f", "", "Path inside the S3 bucket where to put actions. Environment variable: DYN_S3_BUCKET_FOLDER_NAME (required)")
+	rootCmd.PersistentFlags().StringVarP(&s3BucketRegion, "s3-bucket-region", "d", "", "AWS region of the s3 Bucket. Environment variable: DYN_S3_BUCKET_REGION (required)")
+
+	rootCmd.MarkFlagRequired("dynamo-table-name")
+	rootCmd.MarkFlagRequired("dynamo-table-region")
+	rootCmd.MarkFlagRequired("s3-bucket-name")
+	rootCmd.MarkFlagRequired("s3-bucket-region")
+	rootCmd.MarkFlagRequired("s3-bucket-folder-name")
 }
 
 func postInitCommands(commands []*cobra.Command) {
